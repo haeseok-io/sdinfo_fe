f7490c9ad513a09b1854439ef066cacc59883786
haeseok <haeseok_@naver.com>
2026-04-20 21:53:56 +0900

    랭크전 시즌 맵리스트 + 공지사항 컴포넌트 작업

---
+++ src/components/BoardShort.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="boardShort">
+        <h2 class="mb-5 flex items-center justify-between">
+            <p class="font-bold text-2xl"><span class="text-[#2E83C5]">{{ highlight }}</span> {{ subject }}</p>
+            <a :href="`${moreLink}`" class="text-[#B2B2C2]">더보기</a>
+        </h2>
+        <ul class="bg-[#262626] border border-[#3C3C3C] rounded-xl p-4">
+            <li v-for="item in data" :key="item.id" class="flex items-center justify-between mb-2.5 last:mb-0">
+                <p>{{ item.subject }}</p>
+                <p>{{ item.date }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+    const { subject, highlight, moreLink, data } = defineProps({
+        subject: { type: String },
+        highlight: { type: String },
+        moreLink: { type: String, default: "#" },
+        data: { type: Array }
+    })
+</script>
---
+++ src/components/RankMapList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="rankMap">
+        <h2 class="mb-5 font-bold text-2xl"><span class="text-[#2E83C5]">{{ highlight }}</span> {{ subject }}</h2>
+        <ul class="grid grid-cols-6 gap-5">
+            <li v-for="item in data" :key="item.id" class="relative h-[160px] rounded-xl overflow-hidden">
+                <img :src="`/src/assets/images/maps/${item.image}`" :alt="item.title" class="w-full h-full object-cover">
+                <p class="absolute bottom-0 left-0 w-full h-[35px] bg-[#262626] flex items-center justify-center z-10">{{ item.title }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+    const {subject, highlight, data} = defineProps({
+        subject: { type: String },
+        highlight: { type: String },
+        data: Array
+    })
+
+    console.log(data)
+</script>
